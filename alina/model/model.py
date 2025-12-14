58ab73d82ba3dbe0ddaae876f1e13ddb30e7d653
Artem <artem.liamkrow@gmail.com>
2025-12-14 10:07:47 +0300

    dev: reuse layers

--- alina/model/model.py
+++ alina/model/model.py
@@ -1,3 +1,4 @@
+from typing import List
 import math
 import torch
 import torch.nn as nn
@@ -10,18 +11,17 @@ class Model(nn.Module):
                  vocab_size: int,
                  dim: int, 
                  conv_layers: int,
-                 encoder_layers: int,
+                 encoders_order: int | List[int],
                  heads: int,
                  convdrop: float,
                  conv_activation,
-                 norm_layer,
-                 final_heads: int,
-                 final_head_dim: int
+                 norm_layer
                 ):
         super().__init__()
         self.dim = dim
         self.final_dot_norm = math.sqrt(dim)
         
+        # Embedding
         self.embedding = nn.Embedding(vocab_size, dim)
         self.conv_model = nn.Sequential()
         for i in range(conv_layers):
@@ -37,10 +37,20 @@ class Model(nn.Module):
                 torch.nn.init.xavier_uniform_(layer.weight, gain=1.0)
                 self.conv_model.append(layer)
         
+        # Transformer
+        if isinstance(encoders_order, int):
+            encoders_order = list(range(encoders_order))
+
+        assert 0 not in encoders_order, "Encoder indices must start from 1, not 0"
+        assert set(encoders_order) == set(range(1, 1+max(encoders_order))), "All layers must be used"
+
         self.encoders_list = nn.ModuleList()
-        for i in range(encoder_layers):
+        for i in range(max(encoders_order)):
             self.encoders_list.append(EncoderLayer(dim=dim, heads=heads, do=0.1, norm_layer=norm_layer))
 
+        self.encoders_order = [i-1 for i in encoders_order]
+        
+        # Head
         self.final_norm = norm_layer(dim)
         self.DotW = torch.nn.Parameter(torch.rand((dim, dim)), requires_grad=True)
         torch.nn.init.xavier_uniform_(self.DotW, gain=1.0)
@@ -64,12 +74,13 @@ class Model(nn.Module):
         x = self.embedding(seq) # b, seq, dim
         x = torch.transpose(x, 1, 2) # -> b, dim, seq
         x = self.conv_model(x)
-        x = torch.transpose(x, 1, 2) # -> b, seq, dim
+        x = torch.transpose(x, 1, 2) # -> b, seq, dims
 
         x += self.pos_enc(x.size(1), x.size(2)).to(x.dtype).to(x.device)
 
         att_mask = (seq==0).to(x.dtype).view(seq.size(0), 1, 1, seq.size(1)) # b, seq -> b, 1, 1, seq
-        for l in self.encoders_list:
+        for li in self.encoders_order:
+            l = self.encoders_list[li]
             x, scores = l(x, att_mask)
             
         x = self.final_norm(x)
