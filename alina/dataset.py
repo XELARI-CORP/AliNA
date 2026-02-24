bbcf9fba2b44b5d28b0152918b8f2608225fd499
Pavel Podlesnyi <podlesnypr@gmail.com>
2026-02-24 22:21:47 +0300

    feat: add MSAClassifier and MSAClassifierDataset

--- alina/dataset.py
+++ alina/dataset.py
@@ -1,9 +1,14 @@
 import pickle
 import tqdm
 import torch
+import naskit as nsk
+import math
+import numpy as np
 from .dictionary import mono_pair_dict, dimer_pair_dict
+from typing import Iterable
 
-
+class SequenceError(ValueError):
+    pass
 
 class AlinaDataset:
     
@@ -126,7 +131,221 @@ def make_collate(max_len: int, center_pad: bool):
     
     return collate_fn
 
+####  MSAClassifier ####
+class MSAClassifierDataset:
+    def __init__(self, 
+                 data: Iterable[nsk.NucleicAcid],
+                 augment_struct: bool,
+                 msa_sample: float = 1.,
+                 ss_augment_range: tuple[float, float] = (0.2, 0.4),
+                 augment_helix_len_range: tuple[int, int] = (2,4),
+                 
+                 cache: bool = True,
+                 dimer_embeddings: bool = False,
+                 center_pad: bool = False,
+                 max_len: int = 256,
+                 seed: int = 42
+                ):
+            
+        self.data = data
+        for i, na in enumerate(data):
+            for j, seq in enumerate(na.meta['msa']):
+                check_seq(seq, max_len, i, j)
+            
+        self.msa_sample = msa_sample
 
+        self.augment_struct = augment_struct
+        if not self.augment_struct:
+            assert all(["coev_struct" in dp.meta.keys() for dp in data]), "if augment_struct==False, meta must contain 'coev_struct' field"
+        
+        self.ss_augment_range = ss_augment_range
+        self.augment_helix_len_range = augment_helix_len_range
+
+        self._cache = cache
+        self.dimer_embeddings = dimer_embeddings
+        self.center_pad = center_pad
+        self.max_len = max_len
+        self.seed = seed
+        self._rng = np.random.default_rng(seed)
+
+        self.seq2matrix_func = self.dimer_seq2matrix if self.dimer_embeddings else self.mono_seq2matrix
+        self.cache_dtype = torch.uint16 if self.dimer_embeddings else torch.uint8
+    
+    
+    def __len__(self):
+        return len(self.data)
+
+    @staticmethod
+    def dimer_seq2matrix(na):
+        
+        leng = len(na)
+        M = torch.zeros((leng, leng), dtype=torch.int32)
+        for n in range(leng):
+            for p in range(n-1):
+                fx = na[n]
+                fy = na[p]
+    
+                fx1 = ''
+                fy1 = ''
+              
+                if n<leng-1:
+                    fx1 = na[n+1]
+                if p<leng-1:
+                    fy1 = na[p+1]
+                    
+                M[n][p] = dimer_pair_dict[fx+fx1+'/'+fy1+fy]
+                M[p][n] = dimer_pair_dict[fy+fy1+'/'+fx1+fx]
+        return M
+
+    
+    @staticmethod
+    def mono_seq2matrix(na):
+        
+        leng = len(na)
+        M = torch.zeros((leng, leng), dtype=torch.int32)
+        for n in range(leng):
+            for p in range(n-1):
+                fx = na[n]
+                fy = na[p]
+    
+                M[n][p] = mono_pair_dict[fx+fy]
+                M[p][n] = mono_pair_dict[fx+fy]
+        return M
+
+    def seq2tensor(self, na):
+
+        msa = [self.seq2matrix_func(seq).to(self.cache_dtype) for seq in na.meta['msa']]
+        X = torch.zeros((len(msa), self.max_len, self.max_len), dtype=torch.int32)
+        
+        n = msa[0].shape[0]
+        left = (self.max_len - n)//2 if self.center_pad else 0
+        until = (left+n) if self.center_pad else n
+        
+        for i, x in enumerate(msa):
+            X[i, left:until, left:until] = x
+
+        if self.augment_struct:
+            ss = self.augment_ss(na)
+            y = nsk.NA(ss).get_adjacency()
+        else:
+            y = nsk.NA(na.meta['coev_struct']).get_adjacency()
+            
+        y = torch.FloatTensor(y)
+        m = y.shape[0]
+        assert m==n, "length of a sequence in MSA must be equal to SS size"
+        Y = torch.zeros((self.max_len, self.max_len), dtype=torch.float32)
+        Y[left:until, left:until] = y
+        
+        return X, Y
+        
+    def augment_ss(self,
+                   na,
+                   patience = 5):
+        
+        n_max_pairs = len(na) // 2
+        n_pairs = len(na.pairs)
+        
+        freq = self._rng.uniform(*self.ss_augment_range)
+    
+        n_add_pairs = int(np.floor(n_pairs*freq))
+        n_add_pairs = max(1, n_add_pairs)
+    
+        n_pairs = min(n_max_pairs, n_pairs + n_add_pairs)
+        
+        max_compl_ratio = 2*n_pairs/len(na)
+        
+        na = nsk.algo.generate_ss(na,
+                                  min_helix_size=self.augment_helix_len_range[0],
+                                  max_helix_size=self.augment_helix_len_range[0],
+                                  max_compl_ratio=max_compl_ratio,
+                                  patience=patience)
+        
+        compl_ratio = 2*len(na.pairs)/len(na)
+        if(compl_ratio<max_compl_ratio):
+            na = nsk.algo.generate_ss(na,
+                                      min_helix_size=1,
+                                      max_helix_size=1,
+                                      max_compl_ratio=max_compl_ratio,
+                                      patience=patience)
+            
+        return na.struct
+
+    def make_batch(self, key):
+        
+        na = self.data[key]
+        
+        X, adj = self.seq2tensor(na)
+
+        y = na.get_adjacency() if (na.struct is not None) else None
+        
+        return X, adj, y
+
+    def make_msa_sample(self, X):
+
+        n = X.shape[0]
+        k = int(math.ceil(n*self.msa_sample))
+        assert k<=n
+
+        inds = self._rng.choice(n, size=k, replace=False)
+        inds = torch.as_tensor(inds, device=X.device)
+        
+        X = X[inds,:,:]
+
+        return X
+        
+    def __getitem__(self, key: int):
+        if self._cache:
+            if isinstance(self.data[key], tuple):
+                batch = self.data[key]
+            else:
+                batch = self.make_batch(key)
+
+        if self._cache:
+            self.data[key] = batch
+
+        X, adj, y = batch
+        X = self.make_msa_sample(X) # sample msa
+        
+        return X, adj, y
+
+    def precache(self):
+        for i in tqdm.tqdm(range(len(self))):
+            _ = self[i]
+
+    
+    def save(self, path):
+        with open(path, 'wb') as f:
+            pickle.dump({"data":self.data,
+                         "augment_struct":self.augment_struct,
+                         "msa_sample":self.msa_sample,
+                         "ss_augment_range":self.ss_augment_range,
+                         "augment_helix_len_range":self.augment_helix_len_range,
+                         "cache":self._cache,
+                         "dimer_embeddings":self.dimer_embeddings,
+                         "center_pad":self.center_pad,
+                         "max_len":self.max_len,
+                         "seed":self.seed}, f)
+
+    @classmethod
+    def load(cls, path):
+        with open(path, 'rb') as f:
+            data = pickle.load(f)
+
+        ds = cls(**data)
+        return ds
+        
+def check_seq(seq, max_len, i, j):
+    rn = set(seq) - {'A', 'U', 'G', 'C', 'N'}
+    if len(rn)!=0:
+        raise SequenceError(f'msa seq #{j} in dp #{i} contains unknown symbols: {tuple(rn)}')
+    if len(seq)>max_len or len(seq)==0:
+        raise SequenceError(f'msa seq #{j} in dp #{i} out of range (0, 256], got {len(seq)}')
+        
+    
+
+def identity_collate(dps):
+    assert len(dps) == 1
+    return dps[0]
 
 
 
--- alina/model/model.py
+++ alina/model/model.py
@@ -4,7 +4,6 @@ from torch.nn import functional as F
 
 from .modules import pos_enc, ConvLayerNorm, ConvBlock, MHAttention, EncoderLayer
 
-
 class Model(nn.Module):
     def __init__(self, 
                  vocab: int, 
@@ -114,6 +113,164 @@ class Model(nn.Module):
         return x
 
 
+class MSABody(nn.Module):
+    def __init__(self, 
+                 vocab: int, 
+                 emb: int, 
+                 dim: int, 
+                 layers: int, 
+                 heads: int, 
+                 channels: int, 
+                 convdrop: float
+                ):
+        super().__init__()
+        
+        self.embedding = nn.Embedding(vocab, emb)
+        
+        # ENCODER
+        self.pool = nn.MaxPool2d(2, stride=2)
+        self.EncCVBlock1 = ConvBlock(emb, channels[0], convdrop)
+        self.EncCVBlock2 = ConvBlock(channels[0], channels[1], convdrop)
+        self.EncCVBlock3 = ConvBlock(channels[1], channels[2], convdrop)
+        self.EncCVBlock4 = ConvBlock(channels[2], channels[3], convdrop)
+        
+        # MIDDLE
+        self.postpool = nn.Linear(channels[3], dim)
+        torch.nn.init.xavier_uniform_(self.postpool.weight, gain=1.0)
+        
+        self.PE = torch.nn.parameter.Parameter(pos_enc(seq=256, dim=dim), requires_grad=False)
+        self.encoders_list = nn.ModuleList()
+        for i in range(layers):
+            self.encoders_list.append(EncoderLayer(dim=dim, heads=heads, do=0.1))
+
+        # DECODER
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.DecCVBlock1 = ConvBlock((dim+channels[3]), channels[-1], convdrop)
+        self.DecCVBlock2 = ConvBlock(channels[-2]+channels[-1], channels[-2], convdrop)
+        self.DecCVBlock3 = ConvBlock(channels[-3]+channels[-2], channels[-3], convdrop)
+        
+    def get_pad_mask(self, x):
+        x = x.view(-1, 16, 16, 256)
+        x = torch.permute(x, (0, 1, 3, 2)).contiguous() # b, Hpatch, patch_dim, W -> b, Hpatch, W, patch_dim
+        x = x.view(-1, 16**2, 16**2) # b, Hpatch, W, patch_dim -> b, Hpatch*Wpatch, patch_dim*patch_dim
+        
+        x = torch.sum(x, dim=-1, keepdim=False) # b, Hpatch*Wpatch
+        return (x==0.).float()
+        
+            
+    def forward(self, x):
+        mask = self.get_pad_mask(x) # b, seq, seq
+        att_mask = torch.unsqueeze(torch.unsqueeze(mask, 1), 1) # b, seq(1), heads(1), seq
+        
+        # EMB
+        x = self.embedding(x) # b, 256H, 256W, emb
+        x = torch.permute(x, (0, 3, 1, 2)).contiguous() # b, dim, 256H, 256W
+        
+        # ENCODER
+        x1 = self.EncCVBlock1(x) # b, 16, 256, 256 -> b, 32, 256, 256
+        x = self.pool(x1) # b, 32, 256, 256 -> b, 32, 128, 128
+
+        x2 = self.EncCVBlock2(x) # b, 32, 128, 128 -> b, 64, 128, 128
+        x = self.pool(x2) # b, 64, 128, 128 -> b, 64, 64, 64
+
+        x3 = self.EncCVBlock3(x) # b, 64, 64, 64 -> b, 128, 64, 64
+        x = self.pool(x3) # b, 128, 64, 64 -> b, 128, 32, 32
+
+        x4 = self.EncCVBlock4(x) # b, 128, 32, 32 -> b, 256, 32, 32
+        x = self.pool(x4) # b, 256, 32, 32 -> b, 256, 16, 16
+        
+        # MIDDLE
+        x = torch.permute(x, (0, 2, 3, 1)).contiguous() # b, dim, 16, 16 -> b, 16, 16, dim
+        x = x.view(-1, 256, x.shape[-1]) # b, 16, 16, dim -> b, seq(256), dim
+        x = self.postpool(x)
+        x += self.PE
+        
+        for l in self.encoders_list:
+            x = l([x, x, x, att_mask])
+            
+        x = x.view(-1, 16, 16, x.shape[-1])
+        x = torch.permute(x, (0, 3, 1, 2)).contiguous() # b, H, W, dim -> b, dim, H, W
+            
+        # DECODER
+        x = self.upsample(x) # b, 256, 16, 16 -> # b, 256, 32, 32
+        x = torch.cat((x, x4), 1) # b, 256, 32, 32 + b, 256, 32, 32 -> b, 512, 32, 32
+        x = self.DecCVBlock1(x) # b, 512, 32, 32 -> b, 256, 32, 32
+
+        x = self.upsample(x) # b, 256, 32, 32 -> b, 256, 64, 64
+        x = torch.cat((x, x3), 1) # b, 256, 64, 64 + b, 128, 64, 64 -> b, 384, 64, 64
+        x = self.DecCVBlock2(x) # b, 384, 64, 64 -> b, 128, 64, 64
+
+        x = self.upsample(x) # b, 128, 64, 64 -> b, 128, 128, 128
+        x = torch.cat((x, x2), 1) # b, 128, 128, 128 + b, 64, 128, 128 -> b, 192, 128, 128
+        x = self.DecCVBlock3(x) # b, 192, 128, 128 -> b, 64, 128, 128
+
+        x = self.upsample(x) # b, 64, 128, 128 -> b, 64, 256, 256
+        x = torch.cat((x, x1), 1) # b, 64, 256, 256 + b, 32, 256, 256 -> b, 96, 256, 256
+        
+        return x
+
+class MSAClassifier(nn.Module):
+    def __init__(self,
+                 body_parameters: dict,
+                 head_dim: int = 128):
+        super().__init__()
+        self.body = MSABody(**body_parameters)
+        self.head_dim = head_dim
+
+        mlp_input_dim = body_parameters['channels'][-3] + body_parameters['channels'][0] 
+        self.mlp = nn.Sequential(
+            nn.Linear(mlp_input_dim, self.head_dim),
+            nn.SiLU(),
+            nn.Dropout(0.1),
+            nn.Linear(self.head_dim, self.head_dim),
+            nn.SiLU(),
+            nn.Dropout(0.1),
+            nn.Linear(self.head_dim, self.head_dim)
+        )
+        torch.nn.init.kaiming_uniform_(self.mlp[0].weight, nonlinearity='relu')
+        torch.nn.init.kaiming_uniform_(self.mlp[3].weight, nonlinearity='relu')
+        torch.nn.init.xavier_uniform_( self.mlp[6].weight, gain=1.0)
+
+        torch.nn.init.zeros_(self.mlp[0].bias)
+        torch.nn.init.zeros_(self.mlp[3].bias)
+        torch.nn.init.zeros_(self.mlp[6].bias)
+
+        self.head = nn.Sequential(
+            nn.Linear(self.head_dim+1, 2*self.head_dim),
+            nn.SiLU(),
+            nn.Dropout(0.1),
+            nn.Linear(2*self.head_dim, 2*self.head_dim),
+            nn.SiLU(),
+            nn.Dropout(0.1),
+            nn.Linear(2*self.head_dim, 1),
+            nn.Sigmoid()
+        )
+        torch.nn.init.kaiming_uniform_(self.head[0].weight, nonlinearity='relu')
+        torch.nn.init.kaiming_uniform_(self.head[3].weight, nonlinearity='relu')
+        torch.nn.init.xavier_uniform_( self.head[6].weight, gain=1.0)
+
+        torch.nn.init.zeros_(self.head[0].bias)
+        torch.nn.init.zeros_(self.head[3].bias)
+        torch.nn.init.zeros_(self.head[6].bias)
+        
+    def forward(self, msa, adj): # MSA, RNA 2d adj. matrix
+
+        msa = self.body(msa) # b, dim, H, W
+        msa = torch.permute(msa, (0, 2, 3, 1)).contiguous() # b, H, W, dim
+        msa = self.mlp(msa) # b, H, W, head_dim
+
+        msa = msa.sum(axis=0).squeeze() # H, W, head_dim
+
+        adj = adj.unsqueeze(-1).to(msa.dtype) # H, W, 1
+        x = torch.cat((msa, adj), dim=-1) # H, W, head_dim + 1
+
+        x = self.head(x)
+
+        return(x)
+        
+        
+        
+        
 
 
 
