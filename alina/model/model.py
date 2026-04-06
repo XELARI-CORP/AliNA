2835c0a8b5089bd28af7f11035d9eacba99d4735
Pavel Podlesnyi <podlesnypr@gmail.com>
2026-04-06 12:14:30 +0300

    feat: add msa transformer

--- alina/model/model.py
+++ alina/model/model.py
@@ -3,14 +3,16 @@ import math
 import torch
 import torch.nn as nn
 
-from .modules import ComplementaryLayer, EncoderLayer
+from .modules import ComplementaryLayer, EncoderLayer, MSATransformer, make_transformer_block
 
 
 class Model(nn.Module):
     def __init__(self,
                  vocab_size: int,
                  dim: int, 
+                 seq_len: int,
                  conv_layers: int,
+                 msa_transformers_order: int | List[int],
                  encoders_order: int | List[int],
                  heads: int,
                  convdrop: float,
@@ -19,10 +21,13 @@ class Model(nn.Module):
                 ):
         super().__init__()
         self.dim = dim
-        self.final_dot_norm = math.sqrt(dim)
+        self.final_dot_norm = math.sqrt(dim) # ???
         
         # Embedding
         self.embedding = nn.Embedding(vocab_size, dim)
+        self.nb_token = torch.nn.Parameter(torch.rand(1,1,dim), requires_grad=True) # NoBound token 
+
+        # Conv Block
         self.conv_model = nn.Sequential()
         for i in range(conv_layers):
             if i>0:
@@ -40,18 +45,17 @@ class Model(nn.Module):
         # Secondary structure
         self.complementary_layer = ComplementaryLayer(dim)
         
-        # Transformer
-        if isinstance(encoders_order, int):
-            encoders_order = list(range(encoders_order))
+        # MSA Transformer Block
+        self.msa_transformers_block, self.msa_transformers_order = make_transformer_block(
+            msa_transformers_order, MSATransformer,
+            (dim, heads, seq_len, 0.1, norm_layer))
+        
+        # Encoder Block
+        self.encoders_block, self.encoders_order = make_transformer_block(
+            encoders_order, EncoderLayer,
+            (dim, heads, seq_len, 0.1, norm_layer))
 
-        assert 0 not in encoders_order, "Encoder indices must start from 1, not 0"
-        assert set(encoders_order) == set(range(1, 1+max(encoders_order))), "All layers must be used"
-
-        self.encoders_list = nn.ModuleList()
-        for i in range(max(encoders_order)):
-            self.encoders_list.append(EncoderLayer(dim=dim, heads=heads, do=0.1, norm_layer=norm_layer))
-
-        self.encoders_order = [i-1 for i in encoders_order]
+        self._init_pos_enc(seq_len+1, dim)
         
         # Head
         self.final_norm = norm_layer(dim)
@@ -59,42 +63,71 @@ class Model(nn.Module):
         torch.nn.init.xavier_uniform_(self.DotW, gain=1.0)
         self.final_bias = torch.nn.Parameter(torch.tensor(-3.0), requires_grad=True)
 
-
-    @torch.compile
-    def pos_enc(self, seq: int, dim: int):
+    def _init_pos_enc(self, seq: int, dim: int):
         div  = 10000**(2 * (torch.arange(dim, dtype=torch.float32) // 2) / dim)
         pos_enc = torch.arange(seq, dtype=torch.float32)
         pos_enc = pos_enc.view(seq, 1).repeat(1, dim) / div
         
         pos_enc[1:, 0::2] = torch.sin(pos_enc[1:, 0::2]) # dim 2i
         pos_enc[1:, 1::2] = torch.cos(pos_enc[1:, 1::2]) # dim 2i+1
+        
+        self.register_buffer('pe', pos_enc)
 
-        return pos_enc
+    # @torch.compile
+    # def pos_enc(self, seq: int, dim: int):
+    #     div  = 10000**(2 * (torch.arange(dim, dtype=torch.float32) // 2) / dim)
+    #     pos_enc = torch.arange(seq, dtype=torch.float32)
+    #     pos_enc = pos_enc.view(seq, 1).repeat(1, dim) / div
+        
+    #     pos_enc[1:, 0::2] = torch.sin(pos_enc[1:, 0::2]) # dim 2i
+    #     pos_enc[1:, 1::2] = torch.cos(pos_enc[1:, 1::2]) # dim 2i+1
+
+    #     return pos_enc
         
             
     def forward(self, seq: torch.Tensor, struct_vec: torch.Tensor): 
+        batch, msa_len, seq_len = seq.shape
+        x = self.embedding(seq) # b, msa, seq, dim
         
-        x = self.embedding(seq) # b, seq, dim
-        x = torch.transpose(x, 1, 2) # -> b, dim, seq
+        x = x.view(batch*msa_len, seq_len, self.dim).transpose(1,2) # b+msa, dim, seq
         x = self.conv_model(x)
-        x = torch.transpose(x, 1, 2) # -> b, seq, dims
+        x = x.transpose(1,2).reshape(batch, msa_len, seq_len, self.dim) # b, msa, seq, dim
 
-        x += self.pos_enc(x.size(1), x.size(2)).to(x.dtype).to(x.device)
+        seq_mask = (seq==0).to(x.dtype).view(-1, 1, 1, seq_len) # b, msa, seq -> b+msa, 1, 1, seq
+        msa_mask = (seq.sum(dim=-1) == 0).to(x.dtype) # b, msa
+        # b, msa -> b, 1, msa -> b, seq, msa -> b*seq, 1, 1, msa
+        msa_mask = msa_mask.unsqueeze(1).expand(-1, seq_len, -1).reshape(batch * seq_len, 1, 1, msa_len)
+        
+        for li in self.msa_transformers_order:
+            l = self.msa_transformers_block[li]
+            x = l(x, msa_mask, seq_mask)
+
+        x = x[:,0,:,:]   # batch, msa, seq, dim -> batch, seq, dim
+        # add nb token
+        nb_token = self.nb_token.expand(batch, 1, -1) # 1,1,dim -> b, 1, dim
+        x = torch.cat((nb_token, x), dim=1) # b, seq+1, dim
+
+        seq = seq[:,0,:] # batch, msa, seq -> b, seq
+        nb_pad = torch.ones((batch, 1), dtype=seq.dtype, device=seq.device) # b,1
+        seq = torch.cat((nb_pad, seq), dim=1) # b, seq+1
+
+        #x += self.pos_enc(x.size(1), x.size(2)).to(x.dtype).to(x.device)
+        x = x + self.pe.to(x.dtype)
         x = self.complementary_layer(x, struct_vec)
 
         att_mask = (seq==0).to(x.dtype).view(seq.size(0), 1, 1, seq.size(1)) # b, seq -> b, 1, 1, seq
         for li in self.encoders_order:
-            l = self.encoders_list[li]
-            x, scores = l(x, att_mask)
-            
+            l = self.encoders_block[li]
+            x, _ = l(x, att_mask) # b,seq+1,dim
+
         x = self.final_norm(x)
         x = torch.matmul(
-            torch.matmul(x, self.DotW), # -> b, seq, dim
+            torch.matmul(x, self.DotW), # @ dim,dim -> b, seq, dim
             torch.transpose(x, 1, 2) # @ b, dim, seq -> b, seq, seq
         )
         x = x / self.final_dot_norm
         x = x + self.final_bias
-        x = x - 1e7 * torch.diag(torch.ones(x.size(1), dtype=x.dtype, device=x.device)).unsqueeze(0)
+        x = x - 1e7 * torch.eye(x.size(1), dtype=x.dtype, device=x.device).unsqueeze(0)
 
         nbn, m = x[:, 0], x[:, 1:] # b, seq | b, seq-1, seq
         nbn = torch.sigmoid(nbn)
--- alina/model/modules.py
+++ alina/model/modules.py
@@ -2,7 +2,26 @@ import math
 import torch
 import torch.nn as nn
 
+def make_transformer_block(transformer_layers_order, transformer_layer,
+                           transformer_layer_hparams):
 
+        dim, heads, seq_len, do, norm = transformer_layer_hparams
+    
+        if isinstance(transformer_layers_order, int):
+            transformer_layers_order = list(range(1, transformer_layers_order+1))
+
+        assert 0 not in transformer_layers_order, "Layer indices must start from 1, not 0"
+        assert set(transformer_layers_order) == set(range(1, 1+max(transformer_layers_order))), "All layers must be used"
+
+        transformers_list = nn.ModuleList()
+        for _ in range(max(transformer_layers_order)):
+            transformers_list.append(transformer_layer(dim=dim, heads=heads,
+                                                       seq_len=seq_len, do=do,
+                                                       norm_layer=norm))
+
+        transformer_layers_order = [i-1 for i in transformer_layers_order]
+
+        return transformers_list, transformer_layers_order
         
 class ComplementaryLayer(nn.Module):
     def __init__(self, dim: int):
@@ -45,51 +64,83 @@ class ComplementaryLayer(nn.Module):
 
 
 class MHAttention(nn.Module):
-    def __init__(self, dim: int, heads: int):
+    def __init__(self, dim: int, heads: int,
+                 seq_len: int, use_rope: bool):
         super().__init__()
         
         self.heads = heads
         self.dim = dim
         self.depth = dim//heads
         self.norm = math.sqrt(self.depth)
+
+        self.use_rope = use_rope
+        self.seq_len = seq_len
         
         self.Q = nn.Linear(dim, dim)
         self.K = nn.Linear(dim, dim)
         self.V = nn.Linear(dim, dim)
         self.O = nn.Linear(dim, dim)
 
+        if self.use_rope:
+            self.prepare_rope()
+        
         for l in [self.Q, self.K, self.V, self.O]:
             torch.nn.init.xavier_uniform_(l.weight, gain=1.0)
             torch.nn.init.zeros_(l.bias)
 
+    def prepare_rope(self):
+        i = torch.arange(0, self.depth, 2) #0,2...,depth-2
+        dim_freq = 10_000 ** (-i / self.depth)
+        pos  = torch.arange(self.seq_len) 
+
+        freq = torch.outer(pos, dim_freq) # seq_len, depth//2
+        freq = torch.cat((freq, freq), dim=-1) # seq_len, depth
         
+        self.rope_sin = freq.sin()[None,None,:,:] # 1,1,seq_len,depth
+        self.rope_cos = freq.cos()[None,None,:,:]
+
+    def swap_dims(self,x):
+        x1, x2 = x.chunk(2, dim=-1)
+        return torch.cat((-x2,x1), dim=-1)
+    
+    
     def forward(self, q, k, v, mask):
-        batch, seq = q.size(0), q.size(1)
+        # if common att, then q.shape() = (b,seq,dim)
+        # if msa att, then q.shape() = (b,_,n,dim) where n ~ msa or seq
+        orig_shape = q.shape
+        batch, n = orig_shape[0], orig_shape[-2]
 
-        # batch, seq, heads, dim
-        q = self.Q(q).view(batch, seq, self.heads, self.depth)
-        k = self.K(k).view(batch, seq, self.heads, self.depth)
-        v = self.V(v).view(batch, seq, self.heads, self.depth)
+        # virt batch (0), n (1), heads (2), depth (3)
+        q = self.Q(q).view(-1, n, self.heads, self.depth)
+        k = self.K(k).view(-1, n, self.heads, self.depth)
+        v = self.V(v).view(-1, n, self.heads, self.depth)
 
-        # batch, heads, seq, dim
-        q = q.permute(0, 2, 1, 3)
-        k = k.permute(0, 2, 3, 1)
-        v = v.permute(0, 2, 1, 3)
+        q = q.permute(0, 2, 1, 3) # b, h, n, d
+        k = k.permute(0, 2, 1, 3) # b(0), h(1), n(2), d(3)
+        v = v.permute(0, 2, 1, 3) # b, h, n, d
+
+        if self.use_rope:
+            cos = self.rope_cos.to(q.dtype).to(q.device)
+            sin = self.rope_sin.to(q.dtype).to(q.device)
+            
+            q = (q * cos) + (self.swap_dims(q) * sin)
+            k = (k * cos) + (self.swap_dims(k) * sin)
+            
+        k = k.permute(0, 1, 3, 2) # b, h, d, n 
         
         #att
-        g = torch.matmul(q, k)
+        g = torch.matmul(q, k) # b, h, n, n
         g /= self.norm
         A = g - mask*1e7
         A = torch.nn.functional.softmax(A, dim=-1)
 
-        att = torch.matmul(A, v) # b,h,s,d
+        att = torch.matmul(A, v) # b, h, n, d
 
-        att = att.permute(0, 2, 1, 3) # b,s,h,d
-        att = att.reshape(batch, seq, self.dim)
+        att = att.permute(0, 2, 1, 3).contiguous() # b, n, h, d
+        att = att.view(*orig_shape) # virt batch, n, head, depth -> batch, _, n, dim
         att = self.O(att)
         
         return att, g
-    
     
 class FFSwiglu(nn.Module):
     def __init__(self, dim: int, do: float):
@@ -117,15 +168,54 @@ class FFSwiglu(nn.Module):
         x = self.l2(x)
 
         return x
-    
 
-class EncoderLayer(nn.Module):
-    def __init__(self, dim: int, heads: int, do: float, norm_layer):
+class MSATransformer(nn.Module):
+    def __init__(self, dim: int, heads: int, seq_len: int, do: float, norm_layer):
         super().__init__()
         self.dim = dim
         self.heads = heads
 
-        self.Att = MHAttention(self.dim, self.heads)
+        self.SeqAtt = MHAttention(dim, heads, seq_len, True)
+        self.MSAAtt = MHAttention(dim, heads, seq_len, False)
+        self.drop = nn.Dropout(do)
+
+        self.norm_layers = nn.ModuleList([norm_layer(dim) for _ in range(3)])
+
+        self.FF = FFSwiglu(dim, do)
+
+    def forward(self, x, msa_mask, seq_mask):
+        # x.shape: b(0), msa(1), seq(2), dim(3)
+        # seq-wise att
+        att = self.norm_layers[0](x)
+        att, _ = self.SeqAtt(att, att, att, seq_mask)
+        att = self.drop(att)
+        x += att
+
+        x = x.permute(0, 2, 1, 3).contiguous() # b, seq, msa, dim
+
+        #print(x.shape, msa_mask.shape) # !!!
+        att = self.norm_layers[1](x)
+        att, _ = self.MSAAtt(att, att, att, msa_mask)
+        att = self.drop(att)
+        x += att
+
+        x = x.permute(0, 2, 1, 3).contiguous() # b, msa, seq, dim
+        
+        ff = self.norm_layers[2](x)
+        ff = self.FF(ff)
+        x += ff
+
+        return x
+        
+        
+
+class EncoderLayer(nn.Module):
+    def __init__(self, dim: int, heads: int, seq_len: int, do: float, norm_layer):
+        super().__init__()
+        self.dim = dim
+        self.heads = heads
+
+        self.Att = MHAttention(dim, heads, seq_len, False)
         self.drop = nn.Dropout(do)
 
         self.norm1 = norm_layer(dim)
